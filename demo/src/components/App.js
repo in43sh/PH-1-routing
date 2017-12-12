c761a69d3241b208a5a243eb810dd9fc6b8e443e
Sergey Sherstobitov <in43sh@gmail.com>
2017-12-12 11:10:30 -0700

    Added routes in App

--- demo/src/components/App.js
+++ demo/src/components/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
       <div>
         This is App.js
+        {/* <Route exact path="/" render={Home} />
+        <Route exact path="/about" render={About} />
+        <Route exact path="/people" render={People} /> */}
+        <Route exact path="/" render={() => <div>Home</div>} />
+        <Route path="/about" render={() => <div>About</div>} />
+        <Route path="/people" render={() => <div>People</div>} />
       </div>
     );
   }
